bb4d0c990f8d11223a101eac4f060aa95c105601
AgentosUA <agentoswork@gmail.com>
2026-04-27 21:11:54 +0300

    feat: display version in footer

--- src/widgets/layout/footer/index.tsx
+++ src/widgets/layout/footer/index.tsx
@@ -3,11 +3,7 @@ import { Social } from '@/features/social/ui';
 export const Footer = () => (
   <footer className="mt-auto flex items-center justify-center border-t border-white/10 bg-black/80 py-4">
     <div className="container flex w-full items-center justify-between gap-4 px-4 text-xs text-zinc-300 max-lg:flex-col max-lg:text-center">
-      <div className="flex flex-col gap-1 text-left max-lg:text-center">
-        <span className="text-[10px] font-semibold uppercase tracking-[0.26em] text-zinc-500">
-          Virtual Tactical Games
-        </span>
-        <span className="text-xs text-zinc-300">Всі права захищені.</span>
+      <div className="flex flex-col gap-1">
         <a
           href="https://github.com/Arma-Serious-Platform"
           target="_blank"
@@ -15,6 +11,13 @@ export const Footer = () => (
           className="text-[11px] text-zinc-400 hover:text-zinc-200 underline underline-offset-2">
           Реалізовано на базі Arma Serious Platform
         </a>
+        <span className="font-light text-xs">v{process.env.NEXT_PUBLIC_APP_VERSION}</span>
+      </div>
+      <div className="flex flex-col gap-1 text-center">
+        <span className="text-[10px] font-semibold uppercase tracking-[0.26em] text-zinc-500">
+          Virtual Tactical Games
+        </span>
+        <span className="text-xs text-zinc-300">Всі права захищені.</span>
       </div>
 
       <Social className="flex items-center justify-center gap-6" />
