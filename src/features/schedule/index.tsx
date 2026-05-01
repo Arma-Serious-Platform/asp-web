58a42c7af40a825c529226b9521cf902a4adeca9
AgentosUA <agentoswork@gmail.com>
2026-05-01 16:54:49 +0300

    fix: schedule text size

--- src/features/schedule/index.tsx
+++ src/features/schedule/index.tsx
@@ -11,12 +11,12 @@ export const ScheduleInfo: FC<{
     <div className={cn('paper px-2 py-1 flex gap-2 items-center ml-4 rounded-lg', className)}>
       <div className="flex gap-2 items-center">
         <CalendarIcon className="size-4 shrink-0" />
-        {version === 'full' && <span className="whitespace-nowrap">П&apos;ятниця та неділя</span>}
+        {version === 'full' && <span className="whitespace-nowrap text-xs">П&apos;ятниця та неділя</span>}
         {version === 'short' && <span className="text-xs whitespace-nowrap">ПТ та НД</span>}
       </div>
-      <div className="flex gap-2 items-center">
+      <div className="flex gap-2 items-center text-xs">
         <Clock4Icon className="size-4 shrink-0" />
-        {version === 'full' && <span className="whitespace-nowrap">19:30 по Києву</span>}
+        {version === 'full' && <span className="whitespace-nowrap text-xs">19:30 по Києву</span>}
         {version === 'short' && <span className="text-xs whitespace-nowrap">19:30</span>}
       </div>
     </div>
