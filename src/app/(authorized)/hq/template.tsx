28f009afcffb177bfa2af2ab189d68f4ad61bb7a
AgentosUA <agentoswork@gmail.com>
2026-05-28 22:32:03 +0300

    fix: hq access

--- src/app/(authorized)/hq/template.tsx
+++ src/app/(authorized)/hq/template.tsx
@@ -2,17 +2,13 @@
 
 import { session } from '@/entities/session/model';
 import { ROUTES } from '@/shared/config/routes';
-import { SideType } from '@/shared/sdk/types';
 import { observer } from 'mobx-react-lite';
 import { redirect } from 'next/navigation';
 
 export default observer(function HqTemplate({ children }: { children: React.ReactNode }) {
   if (!session.isAuthorized) return redirect(ROUTES.auth.login);
 
-  const sideType = session.user?.user?.squad?.side?.type;
-  const canAccessHq = Boolean(session.user?.user?.squad && [SideType.BLUE, SideType.RED].includes(sideType as SideType));
-
-  if (!canAccessHq) return redirect(ROUTES.home);
+  if (!session.canAccessHeadquarters) return redirect(ROUTES.home);
 
   return children;
 });
